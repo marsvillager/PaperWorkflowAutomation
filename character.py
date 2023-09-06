598cb5a4d5005c6f640e1480f5dc6052e89e6ce1
marsvillager <1968643693@qq.com>
2023-09-06 17:07:10 +0800

    update

--- character.py
+++ character.py
@@ -5,15 +5,15 @@ def handle_special_character(title: str) -> str:
     :param title: 带有特殊字符的标题
     :return: 修正后的标题
     """
-    title: str = title.replace(': ', '：')
-    title: str = title.replace('/', ' or ')
-    title: str = title.replace('&#34;', '\'')  # HTML 中双引号编码成 &#34;
+    title: str = title.replace('&#34;', '"')  # HTML 中双引号编码成 &#34;
     title: str = title.replace('&#38;', '&')  # HTML 中 & 编码成 &#38;
     title: str = title.replace('&#181;', 'μ')  # HTML 中 μ 编码成 &#181;
     title: str = title.replace('&#241;', 'ñ')  # HTML 中 ñ 编码成 &#241;
     title: str = title.replace('&#248;', 'ø')  # HTML 中 ø 编码成 &#248;
     title: str = title.replace('&#402;', 'ƒ')  # HTML 中 ø 编码成 &#402;
     title: str = title.replace('&#956;', 'μ')  # HTML 中 μ 编码成 &#956;
+    title: str = title.replace(': ', '：')
+    title: str = title.replace('/', ' or ')
     title: str = title.replace('?', '.')
     title: str = title.replace('!', '.')
     title: str = title.replace('\n', ' ')
